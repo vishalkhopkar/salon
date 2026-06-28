1745ce05f2e08a67f5b09cd2160071b250b454ea
Vishal Khopkar <vishalkhopkar20@gmail.com>
2026-06-27 19:22:16 -0500

    region

--- infra/app.py
+++ infra/app.py
@@ -3,5 +3,5 @@ import aws_cdk as cdk
 from salon_stack import SalonStack
 
 app = cdk.App()
-SalonStack(app, "SalonStack")
+SalonStack(app, "SalonStack", env=cdk.Environment(region="us-east-1"))
 app.synth()
